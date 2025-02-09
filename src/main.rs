c4113ee5a0aa7d5a03a0f18597c3578e6175d41c
sreysus <sreyassabbani@gmail.com>
2025-02-08 20:19:06 -0500

    feat: add new struct `Vector<T, N>`
    - type alias `Point<T>` for `Vector<T, 3>`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-mod ppm;
+use ray_tracing_rs::ppm;
+use ray_tracing_rs::vector::{Point, Vector};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
-    ppm::diagonal_gradient("output.ppm")?;
+    // ppm::diagonal_gradient("output.ppm")?;
+    let v1 = Vector::from([3.0, 4.0]);
+    println!("{}", v1.norm());
+    let v2 = Vector::from([1.0, 1.0]);
+    println!("{:?}", v1 + v2);
+    println!(
+        "{:?}",
+        Point::from([0.0, 0.0, 1.0]).cross(&Point::from([0.0, 1.0, 0.0]))
+    );
     Ok(())
 }
---
+++ src/vector.rs
@@ -0,0 +1,209 @@
+//! Module defining [`Vector<T, N>`], and variants [`Point<T, 3>`], ...
+//! For other variants, see `src/color.rs` and `...`
+
+use crate::number::Numeric;
+use std::{fmt, ops};
+
+pub struct Vector<T, const N: usize> {
+    entries: Box<[T; N]>,
+}
+
+impl<T, const N: usize> fmt::Debug for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(&*self.entries).finish()?;
+        Ok(())
+    }
+}
+
+impl<T, const N: usize> FromIterator<T> for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut collection = Self::_new();
+
+        for (i, ele) in iter.into_iter().enumerate() {
+            collection.entries[i] = ele;
+        }
+
+        collection
+    }
+}
+
+impl<T, const N: usize> From<[T; N]> for Vector<T, N> {
+    fn from(value: [T; N]) -> Self {
+        Self {
+            entries: Box::new(value),
+        }
+    }
+}
+
+impl<T, const N: usize> Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    /// An internal method for instantiating a [`Vector<T, N>`] type.
+    pub(super) fn _new() -> Self {
+        Self {
+            // `core::array::from_fn` avoids `Copy`
+            entries: Box::new(core::array::from_fn(|_| T::default())),
+        }
+    }
+
+    #[inline]
+    pub fn dot(&self, other: &Self) -> T {
+        // TODO: look into monoids for `additive_identity`
+        self.entries
+            .iter()
+            .zip(other.entries.iter())
+            .map(|(&l, &r)| l * r)
+            .fold(T::additive_identity(), |acc, x| acc + x)
+    }
+
+    /// Returns the norm squared of a [`Vector<T, N>`]
+    #[inline]
+    pub fn norm_squared(&self) -> T {
+        self.dot(self)
+    }
+
+    /// Norm/length of a vector
+    #[inline]
+    pub fn norm(&self) -> T {
+        T::from(self.norm_squared().into().sqrt())
+    }
+
+    #[inline]
+    pub fn unit(&self) -> Self {
+        self / self.norm()
+    }
+}
+
+impl<T, const N: usize> ops::Add for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    type Output = Vector<T, N>;
+    #[inline]
+    fn add(self, rhs: Self) -> Self::Output {
+        self.entries
+            .iter()
+            .zip(rhs.entries.iter())
+            .map(|(&l, &r)| l + r)
+            .collect()
+    }
+}
+
+impl<T, const N: usize> ops::AddAssign<T> for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    #[inline]
+    fn add_assign(&mut self, rhs: T) {
+        self.entries.map(|e| e + rhs);
+    }
+}
+
+impl<T, const N: usize> ops::Sub for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    type Output = Vector<T, N>;
+    #[inline]
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.entries
+            .iter()
+            .zip(rhs.entries.iter())
+            .map(|(&l, &r)| l - r)
+            .collect()
+    }
+}
+
+impl<T, const N: usize> ops::SubAssign<T> for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    #[inline]
+    fn sub_assign(&mut self, rhs: T) {
+        self.entries.map(|e| e + rhs);
+    }
+}
+
+// TODO: Left multiplication is blocked by orphan rules
+impl<T, const N: usize> ops::Mul<T> for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    type Output = Vector<T, N>;
+    /// Provides scalar multiplication of [`Vector<T, N>`]
+    #[inline]
+    fn mul(self, rhs: T) -> Self::Output {
+        Vector::from(self.entries.map(|ele| ele * rhs))
+    }
+}
+
+impl<T, const N: usize> ops::MulAssign<T> for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    #[inline]
+    fn mul_assign(&mut self, rhs: T) {
+        self.entries.map(|e| e + rhs);
+    }
+}
+
+impl<T, const N: usize> ops::Div<T> for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    type Output = Vector<T, N>;
+    /// Provides scalar division of [`Vector<T, N>`]
+    #[inline]
+    fn div(self, rhs: T) -> Self::Output {
+        Vector::from(self.entries.map(|ele| ele / rhs))
+    }
+}
+
+impl<'a, T, const N: usize> ops::Div<T> for &'a Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    type Output = Vector<T, N>;
+    /// Provides scalar division of [`&Vector<T, N>`]
+    #[inline]
+    fn div(self, rhs: T) -> Self::Output {
+        Vector::from(self.entries.map(|ele| ele / rhs))
+    }
+}
+
+impl<T, const N: usize> ops::DivAssign<T> for Vector<T, N>
+where
+    T: Numeric<T>,
+{
+    #[inline]
+    fn div_assign(&mut self, rhs: T) {
+        self.entries.map(|e| e / rhs);
+    }
+}
+
+/// Exported type alias for [`Vector<T, 3>`]
+pub type Point<T> = Vector<T, 3>;
+
+impl<T> Point<T>
+where
+    T: Numeric<T>,
+{
+    // Pretty sure cross product only exists in 3 (and 7) dimensions
+    /// Cross product with another 3D vector ([`Point<T>`])
+    pub fn cross(&self, other: &Self) -> Self {
+        // Ouch to cache
+        // TODO: make this better
+        Vector::from([
+            self.entries[1] * other.entries[2] - self.entries[2] * other.entries[1],
+            self.entries[2] * other.entries[0] - self.entries[0] * other.entries[2],
+            self.entries[0] * other.entries[1] - self.entries[1] * other.entries[0],
+        ])
+    }
+}
