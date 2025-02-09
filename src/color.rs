4d15ef047920f9d0ea6f807e5b2179435bbed296
sreysus <sreyassabbani@gmail.com>
2025-02-09 14:36:26 -0500

    refactor: use `Color` struct instead of type alias to `Vector`
    - conflicts include overlapping implementations for `Display`/`Debug`. PPM P3 needs a specific format, necessitating this separate struct

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,28 @@
-use crate::vector::Vector;
+use std::ops;
 
-pub type Color = Vector<f64, 3>;
+#[derive(Clone)]
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Color { r, g, b }
+    }
+}
+
+impl ops::Mul<u8> for Color {
+    type Output = Color;
+    fn mul(self, rhs: u8) -> Self::Output {
+        Color::new(self.r * rhs, self.g * rhs, self.b * rhs)
+    }
+}
+
+impl ops::Add<u8> for Color {
+    type Output = Color;
+    fn add(self, rhs: u8) -> Self::Output {
+        Color::new(self.r + rhs, self.g + rhs, self.b + rhs)
+    }
+}
