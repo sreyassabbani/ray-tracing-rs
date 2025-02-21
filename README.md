20ab467c96daa4539a4c8e92d9f38a5ea0b76c4c
sreysus <sreyassabbani@gmail.com>
2025-02-21 17:30:55 -0500

    refactor: use `examples` Cargo feature, update README

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ I've finished RTIOW in two weekends (and the school days in between).
 
 ![Final Render](assets/renders/rtiow_final_render.png)
 
+### Running Examples
+You can specify `<example-name>` with any example defined in `Cargo.toml`. If you want to see rendering progress, prefix the following with `RUST_LOG=info`.
+```
+  cargo run --example <example-name>
+```
+You can open PPM files on MacOS with `magick output.ppm display:`.
+
 #### Things to Say
 Initially, I wrote my `Vector` struct as generic as possible, employing a `Numeric` trait and supporting `N`-`Vector`s. Moreover, I couldn't even implement `Copy`, so simple arithmetic operations almost always ended up being horrible to write. I learned my lesson after like 5 chapters, when I was looking at other implementations of RTIOW in Rust: I was never going to need `Vector<10, f64>` or even `Vector<3, f32>`! Even doing this for completeness was stupid.
 
---
+++ examples/defocus_blur.rs
@@ -0,0 +1,59 @@
+//! Example use of the ray tracing library
+//!
+//! Render a example image of defocus blur
+
+use ray_tracing_rs::color::Color;
+use ray_tracing_rs::materials::{Dielectric as Glass, Lambertian as Matte, Metal};
+use ray_tracing_rs::objects::Sphere;
+use ray_tracing_rs::vector::Vector;
+use ray_tracing_rs::{Camera, HittableList, ImageOptions, Point};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Set up materials
+    let material_ground = Matte::new(Color::new(0.8, 0.8, 0.0));
+    let material_center = Matte::new(Color::new(0.1, 0.2, 0.5));
+    let material_bubble = Glass::new(1.0 / 1.50);
+    let material_left = Glass::new(1.50);
+    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
+
+    // Set up objects
+    let ground = Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0, material_ground);
+    let center = Sphere::new(Point::new(0.0, 0.0, -1.2), 0.5, material_center);
+    let left = Sphere::new(Point::new(-1.0, 0.0, -1.0), 0.5, material_left);
+    let bubble = Sphere::new(Point::new(-1.0, 0.0, -1.0), 0.4, material_bubble);
+    let right = Sphere::new(Point::new(1.0, 0.0, -1.0), 0.5, material_right);
+
+    // Set up the world
+    let mut world = HittableList::new();
+    world.add(ground)?;
+    world.add(center)?;
+    world.add(left)?;
+    world.add(right)?;
+    world.add(bubble)?;
+
+    // Output image config, aspect ratio 16:9
+    let image = ImageOptions::new(400, 225).antialias(25);
+
+    // Camera
+    let vfov = 20.0;
+    let look_from = Point::new(-2.0, 2.0, 1.0);
+    let look_at = Point::new(0.0, 0.0, -1.0);
+    let up = Vector::new(0.0, 1.0, 0.0);
+    let defocus_angle = 10.0;
+    let focus_dist = 3.4;
+
+    let camera = Camera::new(
+        vfov,
+        defocus_angle,
+        focus_dist,
+        look_from,
+        look_at,
+        up,
+        image,
+        world,
+    )?;
+
+    camera.render("output.ppm")?;
+
+    Ok(())
+}
